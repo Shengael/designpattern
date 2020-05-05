8ed295c608538fd440b42ab4f205b0881989f05c
Shengael <luis@valdez.fr>
2020-05-05 12:21:47 +0200

    state

--- 06.ESGI.DesignPattern.State/TPSolution.cs
+++ 06.ESGI.DesignPattern.State/TPSolution.cs
@@ -1,6 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _06.ESGI.DesignPattern.State
 {
+
+    public class Task
+    {
+
+        public ITaskState State = new TodoState();
+        
+        public string Start()
+        {
+            return State.Start(this);
+        }
+        public string Close()
+        {
+            return State.Close(this);
+        }
+    }
+
+    public class TodoState: ITaskState
+    {
+        public string Start(Task task)
+        {
+            task.State = new InProgressState();
+            return "TODO -> IN PROGRESS";
+            // return new InProgressState();
+        }
+
+        public string Close(Task task)
+        {
+            return "INVALID TRANSITION";
+        }
+    }
+    
+    public class InProgressState: ITaskState
+    {
+        public string Start(Task task)
+        {
+            return "INVALID TRANSITION";
+        }
+
+        public string Close(Task task)
+        {
+            task.State = new ClosedState();
+            return "IN PROGRESS -> CLOSED";
+        }
+    }
+
+    public class ClosedState: ITaskState
+    {
+        public string Start(Task task)
+        {
+            return "INVALID TRANSITION";
+        }
+
+        public string Close(Task task)
+        {
+            return "INVALID TRANSITION";
+        }
+    }
+
+    public interface ITaskState
+    {
+        string Start(Task task);
+        string Close(Task task);
+    }
   
 }
