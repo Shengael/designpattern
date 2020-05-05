73666e6fb01a1fa1936bc5a2898d070a6313df0d
Shengael <luis@valdez.fr>
2020-05-05 10:41:54 +0200

    strategy

--- 05.ESGI.DesignPattern.Strategy/TPSolution.cs
+++ 05.ESGI.DesignPattern.Strategy/TPSolution.cs
@@ -3,5 +3,39 @@ using System.Linq;
 
 namespace _05.ESGI.DesignPattern.Strategy
 {
+    
+    public class CreditCard: IPayement
+    {
+        public string Pay(int amount)
+        {
+            return $"{amount} has been charged to my credit card";
+        }
+    }
+
+    public class Paypal: IPayement
+    {
+        public string Pay(int amount){
+            return $"{amount} has been charged to my paypal account";
+        }
+    }
+    
+    public interface IPayement
+    {
+        string Pay(int amount);
+    }
+
+    public class ShoppingCard
+    {
+        private int amount = 0;
+        public void AddItem(string element, int price)
+        {
+            amount += price;
+        }
+
+        public string Pay(IPayement strategy)
+        {
+            return strategy.Pay(amount);
+        }
+    }
    
 }
